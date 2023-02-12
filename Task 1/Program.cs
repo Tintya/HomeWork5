399f7e237f60b67eafd0cec749bcf94c6482fccf
Tintya <tintya@mail.ru>
2023-02-13 00:37:31 +0500

    Add Task1

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/ 
+
+
+int[] InitArray()
+{
+    int[] result = new int[12];
+    Random rnd = new Random();
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(100, 1000);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+int CheckArray (int[] arr)
+{   
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) 
+        count ++;
+    }
+    return count;
+    
+}
+
+int[] arr = InitArray();
+PrintArray(arr);
+int value = CheckArray(arr);
+System.Console.WriteLine($"Количество четных чисел = {value}");
